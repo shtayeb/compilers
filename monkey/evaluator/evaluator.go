aac92161f68030d9fa0861a666988a5a520666a9
Shahryar Tayeb <shahryar.dev@gmail.com>
2024-02-13 09:51:05 +0430

    evaluate boolean

--- monkey/evaluator/evaluator.go
+++ monkey/evaluator/evaluator.go
@@ -5,6 +5,12 @@ import (
 	"github.com/shtayeb/compilers/monkey/object"
 )
 
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+	NULL  = &object.Null{}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
@@ -13,9 +19,19 @@ func Eval(node ast.Node) object.Object {
 		return Eval(node.Expression)
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 	}
 
 	return nil
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+
+	return FALSE
 }
 
 func evalStatements(stmts []ast.Statement) object.Object {
