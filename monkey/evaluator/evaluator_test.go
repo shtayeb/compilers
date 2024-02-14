52976a78d27de34603f028c39192c175bb2b0f1d
Shahryar Tayeb <shahryar.dev@gmail.com>
2024-02-14 09:49:16 +0430

    added string concatenation

--- monkey/evaluator/evaluator_test.go
+++ monkey/evaluator/evaluator_test.go
@@ -245,6 +245,10 @@ func TestErrorHandling(t *testing.T) {
 			"identifier not found: foobar",
 		},
 		{
+			`"Hello" - "World"`,
+			"unknown operator: STRING - STRING",
+		},
+		{
 			`
 if (10 > 1) {
 if (10 > 1) {
@@ -347,6 +351,18 @@ func TestStringLiteral(t *testing.T) {
 		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
 	}
 
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
 	if str.Value != "Hello World!" {
 		t.Errorf("String has wrong value. got=%q", str.Value)
 	}
