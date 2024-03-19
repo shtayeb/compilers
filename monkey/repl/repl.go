54aa28079dd966a24c63fb1dc83e7c4ffdabc6bb
Shahryar Tayeb <shahryar.dev@gmail.com>
2024-03-19 11:42:26 +0430

    compiler added to monkey repl

--- monkey/repl/repl.go
+++ monkey/repl/repl.go
@@ -5,43 +5,45 @@ import (
 	"fmt"
 	"io"
 
-	"github.com/shtayeb/compilers/monkey/evaluator"
+	"github.com/shtayeb/compilers/monkey/compiler"
 	"github.com/shtayeb/compilers/monkey/lexer"
-	"github.com/shtayeb/compilers/monkey/object"
 	"github.com/shtayeb/compilers/monkey/parser"
+	"github.com/shtayeb/compilers/monkey/vm"
 )
 
 const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
-	env := object.NewEnvironment()
-
 	for {
-		fmt.Printf(PROMPT)
-
+		fmt.Fprintf(out, PROMPT)
 		scanned := scanner.Scan()
 		if !scanned {
 			return
 		}
-
 		line := scanner.Text()
 		l := lexer.New(line)
 		p := parser.New(l)
-
 		program := p.ParseProgram()
 		if len(p.Errors()) != 0 {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-
-		evaluated := evaluator.Eval(program, env)
-		if evaluated != nil {
-
-			io.WriteString(out, evaluated.Inspect())
-			io.WriteString(out, "\n")
-
+		comp := compiler.New()
+		err := comp.Compile(program)
+		if err != nil {
+			fmt.Fprintf(out, "Woops! Compilation failed:\n %s\n", err)
+			continue
 		}
+		machine := vm.New(comp.ByteCode())
+		err = machine.Run()
+		if err != nil {
+			fmt.Fprintf(out, "Woops! Executing bytecode failed:\n %s\n", err)
+			continue
+		}
+		stackTop := machine.StackTop()
+		io.WriteString(out, stackTop.Inspect())
+		io.WriteString(out, "\n")
 	}
 }
 
