f36b0a0e4f32fe7197e174905aa9049fef6cd8bf
Shahryar Tayeb <shahryar.dev@gmail.com>
2024-04-24 11:37:54 +0430

    Hash: Implemented hash datatype

--- monkey/compiler/compiler.go
+++ monkey/compiler/compiler.go
@@ -2,6 +2,7 @@ package compiler
 
 import (
 	"fmt"
+	"sort"
 
 	"github.com/shtayeb/compilers/monkey/ast"
 	"github.com/shtayeb/compilers/monkey/code"
@@ -202,6 +203,29 @@ func (c *Compiler) Compile(node ast.Node) error {
 			return fmt.Errorf("undefined variable %s", node.Value)
 		}
 		c.emit(code.OpGetGlobal, symbol.Index)
+
+	case *ast.HashLiteral:
+		keys := []ast.Expression{}
+		for k := range node.Pairs {
+			keys = append(keys, k)
+		}
+
+		sort.Slice(keys, func(i, j int) bool {
+			return keys[i].String() < keys[i].String()
+		})
+
+		for _, k := range keys {
+			err := c.Compile(k)
+			if err != nil {
+				return err
+			}
+
+			err = c.Compile(node.Pairs[k])
+			if err != nil {
+				return err
+			}
+		}
+		c.emit(code.OpHash, len(node.Pairs)*2)
 	}
 	return nil
 }
