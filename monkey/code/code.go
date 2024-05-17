a5d9bd0d4030d1fba0b14f4e719289abf96cecbf
Shahryar Tayeb <shahryar.dev@gmail.com>
2024-05-17 10:58:16 +0430

    local bindings: opcodes for local bindings

--- monkey/code/code.go
+++ monkey/code/code.go
@@ -41,6 +41,9 @@ const (
 	OpCall
 	OpReturnValue
 	OpReturn
+
+	OpGetLocal
+	OpSetLocal
 )
 
 type Instructions []byte
@@ -87,6 +90,9 @@ var definitions = map[Opcode]*Definition{
 	OpCall:        {"OpCall", []int{}},
 	OpReturnValue: {"OpReturnValue", []int{}},
 	OpReturn:      {"OpReturn", []int{}},
+
+	OpGetLocal: {"OpGetLocal", []int{1}},
+	OpSetLocal: {"OpSetLocal", []int{1}},
 }
 
 func Lookup(op byte) (*Definition, error) {
@@ -118,7 +124,10 @@ func Make(op Opcode, operands ...int) []byte {
 		switch width {
 		case 2:
 			binary.BigEndian.PutUint16(instruction[offset:], uint16(o))
+		case 1:
+			instruction[offset] = byte(o)
 		}
+
 		offset += width
 	}
 
@@ -167,11 +176,17 @@ func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
 		switch width {
 		case 2:
 			operands[i] = int(ReadUint16(ins[offset:]))
+		case 1:
+			operands[i] = int(ReadUint8(ins[offset:]))
 		}
 		offset += width
 	}
 
 	return operands, offset
+}
+
+func ReadUint8(ins Instructions) uint8 {
+	return uint8(ins[0])
 }
 
 func ReadUint16(ins Instructions) uint16 {
